1055f4f9571533fcba2fcddbeed8a8d99278934a
Brigitta  Pap <pap.mbrigitta@gmail.com>
2023-10-12 13:26:51 +0300

    Time: 7 ms (60.02%), Space: 14.3 MB (23.63%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for(auto &str: operations){
+            if(str[1] == '-'){
+                x--;
+            } else {
+                x++;
+            }
+        }
+        return x;
+    }
+};
